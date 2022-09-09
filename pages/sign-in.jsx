f1b5ac4e3c7904d2d7100d61de6743a179e19e1c
rabius-sunny <rabiussunnyc3x@gmail.com>
2022-09-09 21:53:10 +0600

    login fix

--- pages/sign-in.jsx
+++ pages/sign-in.jsx
@@ -143,7 +143,7 @@ export default function Signin() {
 
             <div className='mt-8'>
               {!isOtp && (
-                <form onSubmit={/* onPhoneSubmit */ handleSubmit}>
+                <form onSubmit={onPhoneSubmit /* handleSubmit */}>
                   <div className='mt-6'>
                     <label
                       htmlFor='phone'
