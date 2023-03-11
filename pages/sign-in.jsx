1a5b6ae9c452294c879d5716bef1d0369741024e
rabius-sunny <rabiussunny10@gmail.com>
2023-03-11 16:46:25 +0600

    signin handler activated

--- pages/sign-in.jsx
+++ pages/sign-in.jsx
@@ -149,7 +149,7 @@ export default function Signin() {
 
             <div className='mt-8'>
               {!isOtp && (
-                <form onSubmit={/*onPhoneSubmit */ handleSubmit}>
+                <form onSubmit={/*handleSubmit */ onPhoneSubmit}>
                   <div className='mt-6'>
                     <label
                       htmlFor='phone'
